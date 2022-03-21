d7a7c9f92fac9a9f554ba47540c883c6f8679575
Vivek Sundaray <viveksundaray123@outlook.com>
2022-03-21 16:22:53 +0530

    added support for other operations

--- app.py
+++ app.py
@@ -3,16 +3,32 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST", "GET"])
-def form():
+@app.route("/")
+def home():
+    operations = ["add", "subtract", "multiply", "divide"]
+    return render_template("home.html", operations=operations)
+
+
+@app.route("/<string:operation>")
+def operate(operation):
+    return render_template(f"{operation}.html")
+
+
+@app.route("/result/<string:operation>", methods=["GET", "POST"])
+def getresult(operation):
     if request.method == "POST":
-        num1 = int(request.form['num1'])
-        num2 = int(request.form['num2'])
-        result = num1 + num2
-        return render_template("home.html", result=result)
-    else:
-        return render_template("home.html")
+        num1 = int(request.form["num1"])
+        num2 = int(request.form["num2"])
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        else:
+            result = round(num1 / num2, 2)
+        return render_template("result.html", result=result)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
